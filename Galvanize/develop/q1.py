44caf7d6fd0ba08729748106f30e63e4f5faf8d6
caheredia <cristianheredia@gmail.com>
2017-10-25 02:34:54 -0700

    played with interpreter

--- Galvanize/develop/q1.py
+++ Galvanize/develop/q1.py
@@ -13,7 +13,9 @@ def main():
     
     text_file  = input("Enter text file, with no quotes (e.g. sample.txt): ")   #name of file to read 
     
-    df = pd.read_csv(text_file, delim_whitespace=True, header = None).T #load into dataframe to utilize pandas operations 
+    df = pd.read_csv('../data/' + text_file, delim_whitespace=True, header = 
+    None).T 
+    #load into dataframe to utilize pandas operations 
     df.columns = ['words'] #rename column
     df.words = df.words.str.replace('[^\w\s]','') #remove special charectors
     df.words = df.words.str.lower(); #convert to lower case 
@@ -21,8 +23,10 @@ def main():
     word_count = len(df)
     unique_words = len(df.words.unique()) 
     word_counts = df.words.value_counts() 
-    
-    sentences = open(text_file, 'r').read().count('.') #counts the number of periods to determine number of sentences
+   
+    #counts the number of periods to determine number of sentences
+    sentences = open('../data/'+text_file, 'r').read().count('.') 
+
 
     
     print('1. The total word count: ', word_count )
